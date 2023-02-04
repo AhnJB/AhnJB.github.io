d859a9cdd3e70b7d39a9505a86bc3a9d71ec07f1
AhnJB <84900093+AhnJB@users.noreply.github.com>
2023-02-04 20:28:51 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,8 @@
 const images = ["0.jpg","1.jpg","2.jpg"]
-
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+document.body.style.backgroundImage = `url('img/${chosenImage}')`
+//const bgImages = document.createElement("img");
 
-const bgImages = document.createElement("img");
+//bgImages.src = `img/${chosenImage}`;
 
-bgImages.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImages);
+//document.body.appendChild(bgImages);
