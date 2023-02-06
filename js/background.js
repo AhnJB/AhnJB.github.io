01fcff1c39dc6f9bf00fb8acfa20e70573204e25
AhnJB <84900093+AhnJB@users.noreply.github.com>
2023-02-06 15:41:17 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.JPG","1.JPG","2.JPG","3.JPG"]
+const images = ["0.PNG","1.PNG","2.PNG","3.PNG","4.PNG"]
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 document.body.style.backgroundImage = `url('img/${chosenImage}')`
 //const bgImages = document.createElement("img");
